c0543186d65bf316c3074f3d38b550f193def516
udan-insith <udaninsith23613@gmail.com>
2026-02-12 09:15:12 +0530

    its done

--- app.js
+++ app.js
@@ -30,4 +30,41 @@ window.addEventListener('scroll', function() {
     } else {
         navabar.classList.remove("fixed-nav")
     }
+
+
+    if (scrollHeight > 500) {
+        topLink.classList.add('show-link')
+    } else {
+        topLink.classList.remove('show-link')
+    }
+})
+
+const scrollLinks = document.querySelectorAll('.scroll-link')
+
+scrollLinks.forEach(function(link) {
+    link.addEventListener("click", function(e) {
+        // prevent default
+        e.preventDefault()
+        // navigate to specific spot
+        const id = e.currentTarget.getAttribute("href").slice(1)
+        const element = document.getElementById(id)
+        // calculate the heights
+        const navHeight = navabar.getBoundingClientRect().height
+        const containerHeight = linksContainer.getBoundingClientRect().height
+        const fixedNav = navabar.classList.contains("fixed-nav")
+        let position = element.offsetTop - navHeight
+
+        if (!fixedNav) {
+            position = position - navHeight
+        }
+        if (navHeight > 82) {
+            position = position + containerHeight
+        }
+
+        window.scrollTo({
+            left: 0,
+            top: position,
+        })
+        linksContainer.style.height = 0
+    })
 })
