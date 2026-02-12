1122acd22c970c44f5b851174291d0473f9b5ead
udan-insith <udaninsith23613@gmail.com>
2026-02-12 07:33:04 +0530

    halfway done

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
+//           footer
 const date = document.getElementById("date")
 date.innerHTML = new Date().getFullYear()
 
+//           close links
 const navToggle = document.querySelector(".nav-toggle")
 const linksContainer = document.querySelector(".links-container")
 const links = document.querySelector(".links")
@@ -9,10 +11,23 @@ navToggle.addEventListener("click", function() {
     // linksContainer.classList.toggle("show-links")
     const containerHeight = linksContainer.getBoundingClientRect().height
     const linksHeight = links.getBoundingClientRect().height
-    
+
     if (containerHeight === 0) {
         linksContainer.style.height = `${linksHeight}px`
     } else {
         linksContainer.style.height = 0
+    }
+})
+
+const navabar = document.getElementById("nav")
+const topLink = document.querySelector(".top-link")
+//         fixed navbar
+window.addEventListener('scroll', function() {
+    const scrollHeight = window.pageYOffset
+    const navHeight = navabar.getBoundingClientRect().height
+    if(scrollHeight > navHeight) {
+        navabar.classList.add("fixed-nav")
+    } else {
+        navabar.classList.remove("fixed-nav")
     }
 })
